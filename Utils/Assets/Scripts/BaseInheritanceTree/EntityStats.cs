1e2fdb1ce3275a6824efba08abe2c9726a541878
Robert Piwowarek <robpiwowarek@gmail.com>
2017-07-22 17:15:20 +0200

    some random empty base classes

---
+++ Utils/Assets/Scripts/BaseInheritanceTree/EntityStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityStats
+{
+    float health;
+    float armour;
+    float speed;
+    bool immune = false;
+
+    List<SpeedImpairEffect> speedModifiers;
+
+    public bool isImmune()
+    {
+        return immune;
+    }
+
+    public void setImmune(bool immune)
+    {
+        this.immune = immune;
+    }
+   
+    public bool isDead()
+    {
+        return health <= 0.0f;
+    }
+
+    public bool hasArmour()
+    {
+        return armour <= 0.0f;
+    }
+
+    public float SpeedWithModifiers()
+    {
+        float speedWithModifiers = 1.0f;
+
+        foreach (SpeedImpairEffect speedImpairEffect in speedModifiers)
+        {
+            speedWithModifiers *= speedImpairEffect.getValue();
+        }
+
+        return speedWithModifiers;
+    }
+
+}
---
+++ Utils/Assets/Scripts/BaseInheritanceTree/SpeedImpairEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedImpairEffect
+{
+    float value = 0.0f;
+    const float MAX_MODIFIER = 2.0f;
+    const float MIN_MODIFIER = 0.0f;
+
+    public void setValue(float val)
+    {
+        if (val <= MIN_MODIFIER) value = MIN_MODIFIER;
+        else if (val >= MAX_MODIFIER) value = MAX_MODIFIER;
+        else
+            value = val;
+    }
+
+    public float getValue()
+    {
+        return value;
+    }
+}
